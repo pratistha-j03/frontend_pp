dd1894b80d9803ee5f322be3dff62f15ab18f86b
Anjali Chaudhary <anjalichaudhary983865@gmail.com>
2026-07-11 12:35:42 +0530

    done

--- src/components/landingpage/CtaBtns.jsx
+++ src/components/landingpage/CtaBtns.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const CTAButtons = () => {
   return (
     <section className="py-4">
-      <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+      <div className="flex flex-row justify-center items-center gap-4">
         {/* Our Services */}
         <Link to="/services">
           <button className="group bg-[#02152E] border-2 border-[#02152E] text-white text-base md:text-lg font-semibold px-5 py-2.5 rounded-xl flex items-center gap-2 shadow-md hover:bg-[#FFD200] hover:border-[#FFD200] hover:text-black transition-all duration-300 cursor-pointer">
