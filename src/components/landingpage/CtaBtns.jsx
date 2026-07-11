ccb8b02753242f1a77d3c1e89fc1aa9f64c4fe7a
Anjali Chaudhary <anjalichaudhary983865@gmail.com>
2026-07-11 05:57:43 +0530

    ...

--- src/components/landingpage/CtaBtns.jsx
+++ src/components/landingpage/CtaBtns.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { ArrowRight } from "lucide-react";
 
 const CTAButtons = () => {
   return (
@@ -9,10 +8,7 @@ const CTAButtons = () => {
         <Link to="/services">
           <button className="group bg-[#02152E] border-2 border-[#02152E] text-white text-base md:text-lg font-semibold px-5 py-2.5 rounded-xl flex items-center gap-2 shadow-md hover:bg-[#FFD200] hover:border-[#FFD200] hover:text-black transition-all duration-300 cursor-pointer">
             Our Services
-            <ArrowRight
-              size={18}
-              className="transition-transform duration-300 group-hover:translate-x-1"
-            />
+            
           </button>
         </Link>
 
@@ -20,10 +16,7 @@ const CTAButtons = () => {
         <Link to="/contact">
           <button className="group bg-[#02152E] border-2 border-[#02152E] text-white text-base md:text-lg font-semibold px-5 py-2.5 rounded-xl flex items-center gap-2 shadow-md hover:bg-[#FFD200] hover:border-[#FFD200] hover:text-black transition-all duration-300 cursor-pointer">
             Contact Us
-            <ArrowRight
-              size={18}
-              className="transition-transform duration-300 group-hover:translate-x-1"
-            />
+            
           </button>
         </Link>
       </div>
