6a27f38a1e5767dba8534ad07b7b6349b5a36d24
Anjali Chaudhary <anjalichaudhary983865@gmail.com>
2026-07-11 12:45:54 +0530

    fixed service card

--- src/components/landingpage/OurServices.jsx
+++ src/components/landingpage/OurServices.jsx
@@ -1,63 +1,31 @@
-import { useState, useEffect } from 'react';
-import ServiceCard from './ServiceCard.jsx';
-import { fetchServices } from '../../api.js';
-// import icon1 from '../../assets/icon4.png';
-// import icon2 from '../../assets/icon3.png';
-// import icon3 from '../../assets/icon2.png';
-// import icon4 from '../../assets/icon1.png';
+import { useState, useEffect } from "react";
+import ServiceCard from "./ServiceCard.jsx";
+import { fetchServices } from "../../api.js";
 
 function Services() {
-  // const servicesData = [
-  //   {
-  //     id: 1,
-  //     icon: icon1,
-  //     title: "Brochure Printing",
-  //     description: "High quality brochures for your business promotions",
-  //   },
-  //   {
-  //     id: 2,
-  //     icon: icon2,
-  //     title: "Visiting Cards",
-  //     description: "High quality brochures for your business promotions",
-  //   },
-  //   {
-  //     id: 3,
-  //     icon: icon3,
-  //     title: "Calendar",
-  //     description: "High quality brochures for your business promotions",
-  //   },
-  //   {
-  //     id: 4,
-  //     icon: icon4,
-  //     title: "Poster Printing",
-  //     description: "High quality brochures for your business promotions",
-  //   },
-  //   {
-  //     id: 5,
-  //     icon: icon1,
-  //     title: "Custom Stickers",
-  //     description: "High quality brochures for your business promotions",
-  //   },
-  // ];
-
   const [servicesData, setServicesData] = useState([]);
 
   useEffect(() => {
     fetchServices()
-      .then(data => setServicesData(data))
-      .catch(err => console.error("Could not fetch active services:", err));
+      .then((data) => setServicesData(data))
+      .catch((err) =>
+        console.error("Could not fetch active services:", err)
+      );
   }, []);
 
   return (
-    <section className="w-screen bg-white py-10 px-16 md:px-12 lg:px-16">
+    <section className="w-full bg-white py-10 px-3 sm:px-6 md:px-12 lg:px-16">
+      
       <div className="text-center mb-12">
         <h4 className="text-[#1A49C9] font-semibold uppercase text-3xl">
           What We Offer
         </h4>
-        <h4 className="text-[#021335] md:text-6xl text-5xl font-bold mt-1 mb-4 tracking-tight">
+
+        <h4 className="text-[#021335] text-5xl md:text-6xl font-bold mt-1 mb-4 tracking-tight">
           Our Services
         </h4>
       </div>
+
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-8 mt-16">
         {servicesData.map((service) => (
           <ServiceCard
@@ -68,6 +36,7 @@ function Services() {
           />
         ))}
       </div>
+
     </section>
   );
 }
