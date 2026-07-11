2a4ed0449b3290adefbe4b7cffb746f2a1c2f95c
Anjali Chaudhary <anjalichaudhary983865@gmail.com>
2026-07-11 11:50:08 +0530

    .

--- src/components/landingpage/AboutSection.jsx
+++ src/components/landingpage/AboutSection.jsx
@@ -51,7 +51,7 @@ function AboutSection() {
           {/* Button */}
           <div className="w-full flex justify-center lg:justify-start">
             <a
-              href="/contact"
+              href="/services"
               className="inline-flex items-center justify-center bg-[#1A49C9] text-white text-lg md:text-xl font-bold px-6 py-3 rounded-xl hover:bg-blue-900 transition-colors duration-200 shadow-md"
             >
               Learn More
